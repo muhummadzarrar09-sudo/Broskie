2837c84f246168867bb6772fa486d97f0ea58cc2
muhummadzarrar09-sudo <266341933+muhummadzarrar09-sudo@users.noreply.github.com>
2026-08-17 15:18:23 +0000

    fix: remove analyzer-reported redundant imports
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- lib/game/broskie_game.dart
+++ lib/game/broskie_game.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 
-import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'components/hazards.dart';
